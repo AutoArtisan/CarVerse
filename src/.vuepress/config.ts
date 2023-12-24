51046703fdefaa6663e5cc38a301dc587168d529
yeekit.zhang@hotmail.com <yeekit.zhang@hotmail.com>
2023-12-24 22:40:11 +0800

    修改路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import theme from "./theme.js";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/CarVerse/",
 
   lang: "zh-CN",
   title: "文档演示",
