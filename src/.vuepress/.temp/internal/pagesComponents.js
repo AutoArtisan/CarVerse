f5d2a2b12613e0566c887bece1e8ac27a79b2ef0
yeekit <>
2024-01-22 20:53:08 +0800

    品牌

--- src/.vuepress/.temp/internal/pagesComponents.js
+++ src/.vuepress/.temp/internal/pagesComponents.js
@@ -11,6 +11,8 @@ export const pagesComponents = {
   "v-1472066c": defineAsyncComponent(() => import(/* webpackChunkName: "v-1472066c" */"/Users/yeekit/Documents/workspace/VuePress/CarVerse/src/.vuepress/.temp/pages/data/index.html.vue")),
   // path: /data/%E7%94%B5%E5%8A%A8%E8%BD%A6%E8%87%AA%E7%87%83%E7%BB%9F%E8%AE%A1.html
   "v-e8ab0dd8": defineAsyncComponent(() => import(/* webpackChunkName: "v-e8ab0dd8" */"/Users/yeekit/Documents/workspace/VuePress/CarVerse/src/.vuepress/.temp/pages/data/电动车自燃统计.html.vue")),
+  // path: /gasoline/
+  "v-ed6e4e28": defineAsyncComponent(() => import(/* webpackChunkName: "v-ed6e4e28" */"/Users/yeekit/Documents/workspace/VuePress/CarVerse/src/.vuepress/.temp/pages/gasoline/index.html.vue")),
   // path: /guide/
   "v-fffb8e28": defineAsyncComponent(() => import(/* webpackChunkName: "v-fffb8e28" */"/Users/yeekit/Documents/workspace/VuePress/CarVerse/src/.vuepress/.temp/pages/guide/index.html.vue")),
   // path: /guide/SUV%E8%BF%98%E6%98%AF%E8%BD%BF%E8%BD%A6.html
@@ -21,8 +23,6 @@ export const pagesComponents = {
   "v-4addfa3b": defineAsyncComponent(() => import(/* webpackChunkName: "v-4addfa3b" */"/Users/yeekit/Documents/workspace/VuePress/CarVerse/src/.vuepress/.temp/pages/guide/大车还是小车.html.vue")),
   // path: /guide/%E6%B2%B9%E8%BD%A6%E8%BF%98%E6%98%AF%E7%94%B5%E8%BD%A6.html
   "v-39ca5d8c": defineAsyncComponent(() => import(/* webpackChunkName: "v-39ca5d8c" */"/Users/yeekit/Documents/workspace/VuePress/CarVerse/src/.vuepress/.temp/pages/guide/油车还是电车.html.vue")),
-  // path: /gasoline/
-  "v-ed6e4e28": defineAsyncComponent(() => import(/* webpackChunkName: "v-ed6e4e28" */"/Users/yeekit/Documents/workspace/VuePress/CarVerse/src/.vuepress/.temp/pages/gasoline/index.html.vue")),
   // path: /nevhicle/
   "v-6d7f5acc": defineAsyncComponent(() => import(/* webpackChunkName: "v-6d7f5acc" */"/Users/yeekit/Documents/workspace/VuePress/CarVerse/src/.vuepress/.temp/pages/nevhicle/index.html.vue")),
   // path: /nevhicle/%E7%94%B5%E5%8A%A8%E6%B1%BD%E8%BD%A6%E5%8C%85%E6%8B%AC%E5%93%AA%E4%BA%9B%E7%B1%BB%E5%9E%8B.html
